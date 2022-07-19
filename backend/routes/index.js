910e1ede7c92c41c68157d8337a3c653f184cf13
Angela Dong <angelaadongg@gmail.com>
2022-07-19 15:18:04 -0700

    add user loging backend endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,20 +1,20 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
-const apiRouter = require('./api');
+const sessionRouter = require('./users.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 router.use('/api', apiRouter);
 
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
+router.use(restoreUser);
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
